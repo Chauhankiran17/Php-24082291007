f2c5df81b706a35a130195a16a232008030c9450
Chauhankiran17 <chauhankiran2503@gmail.com>
2026-02-21 11:00:42 +0530

    Add files via upload

---
+++ Practical_1.php
@@ -0,0 +1,19 @@
+<?php
+echo "Welcome to PHP Programming<br>";
+
+$name = "Vivek"; 
+$age = 25; 
+$salary = 50000; 
+$isStudent = true; 
+
+var_dump($name);
+echo "<br>";
+
+var_dump($age);
+echo "<br>";
+
+var_dump($salary);
+echo "<br>";
+
+var_dump($isStudent);
+?
